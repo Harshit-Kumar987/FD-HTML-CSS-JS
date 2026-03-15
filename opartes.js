22947cd00ed170e44c99ffbde4dd0c74f41b4358
Harshit Kumar <harshitkumar4568@gmail.com>
2026-03-15 23:45:14 +0530

    Add files via upload

---
+++ opartes.js
@@ -0,0 +1,25 @@
+let a=12;
+let b= 2;
+// math
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a**b);
+//comparisional opr
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+//==, ===
+let age=26;
+let age_r="26";
+console.log(age==age_r);
+console.log(age===age_r);
+// and operate (&&)
+console.log(true && true);
+console.log(true && false);
+console.log(false && false);
+// or
+console.log(true || true);
+console.log(true || false);
+console.log(false || false);
