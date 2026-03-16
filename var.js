526ea5bb1afa811174f8ba8fd18d1fc70c637bae
Harshit Kumar <harshitkumar4568@gmail.com>
2026-03-16 23:38:42 +0530

    Add files via upload

---
+++ var.js
@@ -0,0 +1,27 @@
+//VARIABLE IN JAVASCRIPT
+//total 3 way - let, var and const
+//Redeclare, Reassign and scope 
+let x= 10;
+console.log(x);
+x= 12; //reassign allowed
+console.log(x);
+//let x= 13; redeclaration not allowed
+console.log(x);
+{
+    let y=12;   //block Scope
+}
+//console.log(y);
+//var--
+var a =14;
+console.log(a);
+var a =15;
+console.log(a);
+var a =16;
+console.log(a);
+//const--
+const b=17;
+console.log(b);
+//b=19;
+console.log(b);
+//const b=20;
+console.log(b);
